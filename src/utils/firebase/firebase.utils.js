de7d880dde1b11dfcc645e8eba24122b26e4f07f
victorproenca1 <victorproenca2020@gmail.com>
2022-06-03 16:05:53 -0300

    first commit: react website base and firebase config

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,54 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDvyrAOa5-epZB_Gox0PXjL9_p0RzK5oQI",
+    authDomain: "crwn-shopp-db.firebaseapp.com",
+    projectId: "crwn-shopp-db",
+    storageBucket: "crwn-shopp-db.appspot.com",
+    messagingSenderId: "364475116090",
+    appId: "1:364475116090:web:395f8f6cbb71544657e303"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch (error) {
+            console.log('Error creating the user: ', error.message);
+        }
+    }
+    return userDocRef;
+};
